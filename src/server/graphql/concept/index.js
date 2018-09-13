3ca46978841fcd1288e238069ce5ae8750e76edb
Mavriiick42 <clownclowed2@gmail.com>
2018-09-13 18:54:22 +0800

    Add concept to database/graphql

---
+++ src/server/graphql/concept/index.js
@@ -0,0 +1,44 @@
+const firebase = require('firebase');
+const { GraphQLObjectType, GraphQLString, GraphQLList } = require('graphql');
+
+const itemConceptType = new GraphQLObjectType({
+  name: 'itemConceptType',
+  fields: () => ({
+    picture: {
+      type: GraphQLString,
+      resolve: ({ picture }) => picture,
+    },
+    title: {
+      type: GraphQLString,
+      resolve: ({ title }) => title,
+    },
+  }),
+});
+
+const conceptType = new GraphQLObjectType({
+  name: 'conceptType',
+  fields: () => ({
+    concepts: {
+      type: GraphQLList(itemConceptType),
+    },
+    subtitle: {
+      type: GraphQLString,
+      resolve: ({ subtitle }) => subtitle,
+    },
+    title: {
+      type: GraphQLString,
+      resolve: ({ title }) => title,
+    },
+  }),
+});
+
+module.exports = {
+  type: conceptType,
+  resolve() {
+    return firebase
+      .database()
+      .ref('/unfinityCMS/concept')
+      .once('value')
+      .then((res) => res.val());
+  },
+};
--- src/server/graphql/schema.js
+++ src/server/graphql/schema.js
@@ -1,11 +1,13 @@
 const { GraphQLSchema, GraphQLObjectType } = require('graphql');
 const header = require('./header');
+const concept = require('./concept');
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
       header,
+      concept,
     },
   }),
 });
