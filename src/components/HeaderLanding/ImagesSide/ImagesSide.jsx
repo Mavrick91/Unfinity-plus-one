dd4f317ca4732cd84673f36152adf9ae7bcdae26
Mavriiick42 <clownclowed2@gmail.com>
2018-09-14 20:14:05 +0800

    Make header responsive

--- src/components/HeaderLanding/ImagesSide/ImagesSide.jsx
+++ src/components/HeaderLanding/ImagesSide/ImagesSide.jsx
@@ -4,33 +4,34 @@ import React, { PureComponent } from 'react';
 import './ImagesSide.css';
 
 type Props = {
-  pictures: Array<string>,
+  picture: string,
+  top: string,
+  left: string,
+  zIndex: string,
 };
 
 export default class ImagesSide extends PureComponent<Props> {
-  displayPictures = (pictures: Array<string>) => (
-    <div className="images_wrapper">
-      {pictures.map((picture, index) => (
+  render() {
+    const { picture, top, left, zIndex } = this.props;
+
+    return (
+      <div
+        className="images_container"
+        style={{
+          left,
+          top,
+          zIndex,
+        }}
+      >
         <img
-          key={picture}
           src={picture}
           alt=""
           style={{
-            marginLeft: `calc(80px * ${-index})`,
-            marginBottom: `calc(80px * ${-index})`,
             boxShadow: '5px 5px 10px #111',
             height: '300px',
           }}
         />
-      ))}
-    </div>
-  );
-
-  render() {
-    const { pictures } = this.props;
-
-    return (
-      <div className="images_container">{this.displayPictures(pictures)}</div>
+      </div>
     );
   }
 }
